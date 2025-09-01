81804b8eef2eb13217dd4926148ee805b5b8ada4
RafaelUser123 <rafadedeus2020@gmail.com>
2025-09-01 17:11:00 -0300

    Criando pgs

---
+++ compos_to/pages/partnumbers/composto.js
@@ -0,0 +1,3 @@
+export default function Composto(){
+    return <h1> PN: MS00263329 </h1>
+} 
---
+++ compos_to/pages/partnumbers/index.js
@@ -0,0 +1,3 @@
+export default function Partnumbers(){
+    return <h1> Projeto Latam Composto </h1>
+}
